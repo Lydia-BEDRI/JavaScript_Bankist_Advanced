4b390675a3449b40218f051bc4029ba6b6003910
Lady <bedrilydia118@gmail.com>
2024-06-17 19:28:46 +0200

    Building a tabbed component

--- script.js
+++ script.js
@@ -190,3 +190,41 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
+// const h1 = document.querySelector('h1');
+
+// // console.log(h1.querySelectorAll('.highlight'));
+// // console.log(h1.children);
+// // h1.firstElementChild.style.color = 'white';
+// // h1.lastElementChild.style.color = 'orangered';
+
+// // console.log(h1.parentNode);
+
+// // h1.closest('.h1').style.background = 'var(--gradient-primary)';
+
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+
+// Building a Tabbed Component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
