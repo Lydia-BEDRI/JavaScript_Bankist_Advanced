ece1944cc7255cf9f23143c76bbd32550241f086
Lady <bedrilydia118@gmail.com>
2024-06-17 22:03:46 +0200

    Implementing Menu fade animation

--- script.js
+++ script.js
@@ -9,7 +9,11 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
-
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
+//console.log(nav);
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -205,9 +209,7 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 // console.log(h1.nextElementSibling);
 
 // Building a Tabbed Component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
+
 
 
 tabsContainer.addEventListener('click', function (e) {
@@ -228,3 +230,28 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+//Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+    // console.log(link, siblings, logo);
+    siblings.forEach(el => {
+      if (el !== link) {
+        el.style.opacity = this;
+      }
+
+    });
+    logo.style.opacity = this;
+  }
+}
+// nav.addEventListener('mouseover', function (e) {
+//   handleHover(e, 0.5);
+// });
+// nav.addEventListener('mouseout', function (e) {
+//   handleHover(e, 1);
+// });
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
