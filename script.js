ca5c94c45d1f817bad1db552f6959190adfd5505
Lady <bedrilydia118@gmail.com>
2024-06-17 22:14:56 +0200

    Implementing a sticky navigation

--- script.js
+++ script.js
@@ -255,3 +255,16 @@ const handleHover = function (e) {
 
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+//Implementing a sticky navigation 
+
+const initialCoords = section1.getBoundingClientRect();
+window.addEventListener('scroll', function (e) {
+  //console.log(e);
+  // console.log(window.scrollY);
+  if (this.window.scrollY > initialCoords.top) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+});
